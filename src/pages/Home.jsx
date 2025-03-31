6048cc5016fb6f2f85d99fe6f3300a62deb1fbd4
pratik kad <pratikkad22@gmail.com>
2025-03-31 17:55:12 +0530

    isLoading state added with spinner

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,16 +4,18 @@ import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
 import Card from '../components/Card'
 import { getRestaurants } from '../services/getService'
+import Spinner from '../components/Spinner'
 
 function Home() {
-
+  const [isLoading, setIsLoading]=useState(false);
   const [restaurants, setRestaurants] = useState([]);
 
   async function fetchRestaurants(){
       try {
+        setIsLoading(true);
         const data=await getRestaurants();
-        // console.log(data.data.restaurants);
         setRestaurants(data.data.restaurants);
+        setIsLoading(false);
       } catch (error) {
         console.log("Error in fetch restaurants", error.message);
       }
@@ -25,12 +27,14 @@ function Home() {
 
 
   return (
-    <div className='flex justify-center  gap-4 items-center flex-wrap p-4 bg-zinc-300'>
+    <div className='flex justify-center gap-4 items-center flex-wrap p-4 bg-zinc-300'>
 
       {
-        restaurants && restaurants.map((restaurant, index)=>{
-          return <Card restaurant={restaurant} key={index} />
-        })
+        isLoading === true ? (<Spinner/>) : (
+          restaurants.map((restaurant, index)=>{
+            return <Card restaurant={restaurant} key={index} />
+          })
+        )  
       }
 
     </div>
