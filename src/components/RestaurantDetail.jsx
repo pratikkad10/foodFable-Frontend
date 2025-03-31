e4f49963ec1149d1bbc07ae23502e1a04c93fe5c
pratik kad <pratikkad22@gmail.com>
2025-03-31 17:52:36 +0530

    add review component added

--- src/components/RestaurantDetail.jsx
+++ src/components/RestaurantDetail.jsx
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { fetchRestaurantById } from "../services/getService";
+import RatingReview from "./RatingReview";
+import Spinner from "./Spinner";
 
 function RestaurantDetail() {
-
-    
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   const [restaurant, setRestaurant] = useState({});
   const { id } = useParams();
 
   const calculateAverageRating = (reviews) => {
-    if (!reviews || reviews.length === 0) return 'N/A'; // Handle no reviews case
+    if (!reviews || reviews.length === 0) return "N/A"; // Handle no reviews case
     const totalRating = reviews.reduce((sum, review) => sum + review.rating, 0);
     return (totalRating / reviews.length).toFixed(2); // Round to 2 decimal places
   };
@@ -19,9 +20,10 @@ function RestaurantDetail() {
 
   async function fetchRestaurant() {
     try {
+      setIsLoggedIn(true);
       const res = await fetchRestaurantById(id);
-      console.log(res.data.restaurants);
       setRestaurant(res.data.restaurants);
+      setIsLoggedIn(false);
     } catch (error) {
       console.log(error.message);
     }
@@ -32,44 +34,61 @@ function RestaurantDetail() {
   }, []);
 
   return (
-    <div className="bg-zinc-300 min-h-[100vh]  w-auto flex flex-col justify-center items-center">
-      <div className="bg-zinc-200 w-[75%] h-100 flex rounded-lg overflow-hidden">
-        <img  className="rounded-lg w-[50%] h-full " src={restaurant.imageUrl} alt="" />
-        <div className=" px-10 flex flex-col justify-center  ">
-            <div className="text-4xl font-bold py-2 mb-4 text-zinc-900">{restaurant.name}</div>
-            <div className="text-xl text-zinc-800 py-1">{restaurant.city}</div>
-            <div className="text-xl text-zinc-800 py-1">{restaurant.address}</div>
-            <div className="text-xl text-zinc-800 py-1">{restaurant.contact}</div>
-            <div className="text-xl text-zinc-800 py-1">{restaurant.email}</div>
-            <a className='text-blue-500 text-xl ' href={restaurant.website}>{restaurant.website}</a>
-            <div className="text-xl text-zinc-800 py-1">{restaurant.status} now</div>
-            <div className="text-yellow-500 text-xl mt-6 font-bold">★ {averageRating}</div>
-        </div>
-      </div>
+    <>
+      {isLoggedIn === true ? (
+        <Spinner />
+      ) : (
+        <div className="bg-zinc-300 min-h-[100vh] py-4 w-auto flex flex-col justify-center items-center">
+          <div className="bg-zinc-200 w-[75%] h-100 flex rounded-lg overflow-hidden">
+            <img
+              className="rounded-lg w-[50%] h-full "
+              src={restaurant.imageUrl}
+              alt=""
+            />
+            <div className=" px-10 flex flex-col justify-center  ">
+              <div className="text-4xl font-bold py-2 mb-4 text-zinc-900">
+                {restaurant.name}
+              </div>
+              <div className="text-xl text-zinc-800 py-1">
+                {restaurant.city}
+              </div>
+              <div className="text-xl text-zinc-800 py-1">
+                {restaurant.address}
+              </div>
+              <div className="text-xl text-zinc-800 py-1">
+                {restaurant.contact}
+              </div>
+              <div className="text-xl text-zinc-800 py-1">
+                {restaurant.email}
+              </div>
+              <a className="text-blue-500 text-xl " href={restaurant.website}>
+                {restaurant.website}
+              </a>
+              <div className="text-xl text-zinc-800 py-1">
+                {restaurant.status} now
+              </div>
+              <div className="text-yellow-500 text-xl mt-6 font-bold">
+                ★ {averageRating}
+              </div>
+            </div>
+          </div>
 
-      <div className="reviews w-[75%]  mt-4">
-        <h1 className=" text-xl text-zinc-900 font-semibold">Reviews</h1>
-        {
-          restaurant.reviews &&
-          restaurant.reviews.map((review, index)=>{
-            return <Review review={review} key={index} />
-          })
-        }
-      </div>
-    </div>
+          <div className="reviews w-[75%]  mt-4 bg-zinc-200 rounded-lg shadow-lg p-4 my-4">
+            <RatingReview id={id} />
+            <h1 className="text-lg font-semibold ">Reviews</h1>
+            {restaurant.reviews &&
+              restaurant.reviews.map((review, index) => {
+                return (
+                  <li className="font-normal py-1 " key={index}>
+                    {review.comment}
+                  </li>
+                );
+              })}
+          </div>
+        </div>
+      )}
+    </>
   );
 }
-
-function Review({review}){
-  console.log(review);
-  
-  return (
-    
-    <div className=" bg-zinc-200 rounded-2xl my-2 p-4">
-      <div className="text-zinc-800 font-thin">{review.comment}</div>
-    </div>
-  )
-}
-
 
 export default RestaurantDetail;
